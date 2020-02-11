244d6f604d9558b19c953b4ecaa4f47b9ea1c3b3
oldGunCoder <concordia.ie@gmail.com>
2020-02-11 13:32:05 -0500

    homework 1 completed

---
+++ DataStructureTasks/src/tasks/Homework1.java
@@ -0,0 +1,19 @@
+package tasks;
+
+public class Homework1 {
+	
+	/*Given an array of integers, return true if the array is length 1 or more, 
+	and the first element and the last element are equal. */
+	int i;
+	int[] arr = new int[i];
+	
+	public boolean isTrue(int[] arr) {
+		if (arr.length == 0) {
+			return false;
+		} else if (arr[0]==arr[arr.length-1]) {
+			return true; 
+		} 
+		return false;
+	}
+
+}
---
+++ DataStructureTasks/src/tasks/RunProgram.java
@@ -0,0 +1,21 @@
+package tasks;
+
+public class RunProgram {
+	
+	public static void main(String[] args) {
+		Homework1 hw = new Homework1();
+		
+		int[] array1 = new int[] {1,2,3,4,5,6,7,8};
+		int[] array2 = new int[] {0,6,1,5,3,0};
+		int[] array3 = new int[] {0};
+		int[] array4 = new int[] {};
+		
+		System.out.println("array1 meet the conditions? " + hw.isTrue(array1));
+		System.out.println("array2 meet the conditions? " + hw.isTrue(array2));
+		System.out.println("array3 meet the conditions? " + hw.isTrue(array3));
+		System.out.println("array4 meet the conditions? " + hw.isTrue(array4));
+
+		
+
+	}
+}
