ea44b488ffe89e84e08f2854b09570a2ef2f0183
oldGunCoder <concordia.ie@gmail.com>
2020-02-13 00:21:10 -0500

    1 and 2 are done

--- DataStructureTasks/src/tasks/Homework3.java
+++ DataStructureTasks/src/tasks/Homework3.java
@@ -2,4 +2,61 @@ package tasks;
 
 public class Homework3 {
 
+	/*1-
+	Count recursively the total number 
+	of "abc" and "aba" substrings 
+	that appear in the given string.*/
+	public int countSubstring (String str) {
+		//exit condition
+		if (str.length() < 3) {
+			return 0;
+		}
+		
+		//check the count
+		if(str.substring(0, 3).equals("abc")) {
+			return 1 + countSubstring(str.substring(3));
+		} else if (str.subSequence(0, 3).equals("aba")) {
+			return 1 + countSubstring(str.substring(3));
+		}
+		
+		//if it wasn't abc or aba, we removed the first character
+		return countSubstring(str.substring(1));
+	}
+	
+	/*2-
+	Given a string, compute recursively (no loops) 
+	the number of "11" substrings in the string. 
+	The "11" substrings should not overlap.*/
+	public int countElevens(String str) {
+		//exit condition
+		if (str.length() < 2) {
+			return 0;
+		}
+		
+		//check the count
+		if(str.substring(0, 2).equals("11")) {
+			return 1 + countElevens(str.substring(2));
+		} 
+		
+		//if it wasn't 11, we removed the first character
+		return countElevens(str.substring(1));
+	}
+	
+	/*3-
+	Given an array of ints, is it possible to 
+	divide the ints into two groups, so that the 
+	sums of the two groups are the same. 
+	Every int must be in one group or the other. 
+	Write a recursive helper method that takes whatever
+ 	arguments you like, and make the initial call to your 
+	recursive helper from splitArray(). (No loops needed.)
+
+	splitArray([2, 2]) ? true
+	splitArray([2, 3]) ? false
+	splitArray([5, 2, 3]) ? true*/
+	
+	public boolean splitArray(int[] arr) {
+		
+		return true;
+	}
 }
--- DataStructureTasks/src/tasks/RunProgram.java
+++ DataStructureTasks/src/tasks/RunProgram.java
@@ -17,43 +17,52 @@ public class RunProgram {
 //
 //	}
 	
+//	public static void main(String[] args) {
+//		Homework2 hw2 = new Homework2();
+//		
+//		int[] array1 = new int[] {1, 2, 3, 4};
+//		int[] array2 = new int[] {7, 1, 2, 3, 4, 9};
+//		int[] array3 = new int[] {1, 2};
+//		
+//		hw2.makeMiddle(array1);
+//		hw2.makeMiddle(array2);
+//		hw2.makeMiddle(array3);
+//		System.out.println("********************************************");
+//		
+//		int[] array4 = new int[] {};
+//		int[] array5 = new int[] {6, 2, 5, 3};
+//		int[] array6 = new int[] {1,2};
+//		int[] array7 = new int[] {1};
+//		
+//		hw2.fun(array4);
+//		System.out.println();
+//		hw2.fun(array5);
+//		System.out.println();
+//		hw2.fun(array6);
+//		System.out.println();
+//		hw2.fun(array7);
+//		System.out.println();
+//		System.out.println("********************************************");
+//	
+//		int[] array_a1 = new int[] {1,2,3};
+//		int[] array_b1 = new int[] {4,5,6};
+//		int[] array_a2 = new int[] {7,7,7};
+//		int[] array_b2 = new int[] {3,8,0};
+//		int[] array_a3 = new int[] {5,2,9};
+//		int[] array_b3 = new int[] {1,4,5};
+//		hw2.middleWay(array_a1, array_b1);
+//		hw2.middleWay(array_a2, array_b2);
+//		hw2.middleWay(array_a3, array_b3);
+//
+//	}
 	public static void main(String[] args) {
-		Homework2 hw2 = new Homework2();
+		Homework3 hw3 = new Homework3();
 		
-		int[] array1 = new int[] {1, 2, 3, 4};
-		int[] array2 = new int[] {7, 1, 2, 3, 4, 9};
-		int[] array3 = new int[] {1, 2};
-		
-		hw2.makeMiddle(array1);
-		hw2.makeMiddle(array2);
-		hw2.makeMiddle(array3);
+		System.out.println(hw3.countSubstring("gabacabcpppcabcabc"));
+		System.out.println("********************************************");
+		System.out.println(hw3.countElevens("119110114511119"));
 		System.out.println("********************************************");
 		
-		int[] array4 = new int[] {};
-		int[] array5 = new int[] {6, 2, 5, 3};
-		int[] array6 = new int[] {1,2};
-		int[] array7 = new int[] {1};
 		
-		hw2.fun(array4);
-		System.out.println();
-		hw2.fun(array5);
-		System.out.println();
-		hw2.fun(array6);
-		System.out.println();
-		hw2.fun(array7);
-		System.out.println();
-		System.out.println("********************************************");
-	
-		int[] array_a1 = new int[] {1,2,3};
-		int[] array_b1 = new int[] {4,5,6};
-		int[] array_a2 = new int[] {7,7,7};
-		int[] array_b2 = new int[] {3,8,0};
-		int[] array_a3 = new int[] {5,2,9};
-		int[] array_b3 = new int[] {1,4,5};
-		hw2.middleWay(array_a1, array_b1);
-		hw2.middleWay(array_a2, array_b2);
-		hw2.middleWay(array_a3, array_b3);
-
 	}
-	
 }
